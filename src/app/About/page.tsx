d20207f3640d20d6db6afbc3a6bdfd5486afbb82
Ivy Yip <ivysyip@gmail.com>
2024-11-17 00:04:41 -0500

    text for about page complete

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,10 +1,25 @@
 'use client'
 import React from 'react';
+// import { IconArrowRight } from '@tabler/core';
 
 export default function About() {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)] mx-auto px-12 py-4">
+            <h1 className="text-4xl lg:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-400">
+                About Me!
+            </h1>
+            <ul className="text-lg lg:text-xl py-8">
+                {/* <IconArrowRight/> */}
+                <li>Name: Ivy Yip</li>
+                <li>School Year: College Freshman</li>
+                <li>School: Massachusetts Institute of Technology {"(MIT)"}</li>
+                <li>Major: 6-3 {"(Computer Science)"} and 18 {"(Applied Mathematics)"}</li>
+                <li>Hometown: Austin, TX and Hong Kong</li>
+                <li>Hobbies: Traveling, trying new Asian restaurants, watching Formula 1</li>
+                <li>Favorite Food: Sushi and Boba!</li>
+                <li>Languages: English & Chinese {"(Cantonese and Mandarin)"}</li>
+                <li>Instruments: Piano & Liuqin {"(Chinese Instrument)"}</li>
+            </ul>
         </div>
     )
 }
