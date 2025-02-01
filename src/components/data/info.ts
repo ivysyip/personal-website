63c7aedbc2027869e136f3e948aacd0685d83295
Ivy Yip <ivysyip@gmail.com>
2025-02-01 00:19:34 -0500

    info

---
+++ src/components/data/info.ts
@@ -0,0 +1,38 @@
+export const aboutInfo = [
+  {
+    category: "Name",
+    info: "Ivy Yip",
+  },
+  {
+    category: "School Year",
+    info: "College Freshman",
+  },
+  {
+    category: "School",
+    info: "Massachusetts Institute of Technology (MIT)",
+  },
+  {
+    category: "Major",
+    info: "6-3 (Computer Science) and 18 (Applied Mathematics)",
+  },
+  {
+    category: "Hometown",
+    info: "Austin, TX and Hong Kong",
+  },
+  {
+    category: "Hobbies",
+    info: "Traveling, trying new Asian restaurants, watching Formula 1, speedcubing",
+  },
+  {
+    category: "Favorite Food",
+    info: "Sushi and Boba!",
+  },
+  {
+    category: "Languages",
+    info: "English & Chinese (Cantonese and Mandarin)",
+  },
+  {
+    category: "Instruments",
+    info: "Piano & Liuqin (Chinese Instrument)",
+  },
+];
